47178fbf716b8f43f03a30ad2d9cb32b70002240
ricardo-temple <ricardovtemple@gmail.com>
2024-04-09 19:30:50 -0300

    Evento com lista de Tweets

--- src/main/java/com/example/apiSocialRate_v2/controller/dto/TweetDTO.java
+++ src/main/java/com/example/apiSocialRate_v2/controller/dto/TweetDTO.java
@@ -1,13 +1,9 @@
 package com.example.apiSocialRate_v2.controller.dto;
 
-import com.example.apiSocialRate_v2.model.Evento;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class TweetDTO {
-    private Long idEvento;
     private String textTweet;
     private Long likeCountTweet;
     private Long replyCountTweet;
--- src/main/java/com/example/apiSocialRate_v2/model/Evento.java
+++ src/main/java/com/example/apiSocialRate_v2/model/Evento.java
@@ -1,36 +1,34 @@
 package com.example.apiSocialRate_v2.model;
 
 import com.example.apiSocialRate_v2.controller.dto.EventoDTO;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.example.apiSocialRate_v2.controller.dto.TweetDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+
+import java.util.List;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Evento {
     @Id
     @GeneratedValue
     private Long idEvento;
     private String nomeEvento;
     private String setorEvento;
-
-    //Construtor vazio
-    public Evento(){
-
-    }
-
-    //Construtor cheio
-    public Evento(Long idEvento, String nomeEvento, String setorEvento){
-        this.idEvento = idEvento;
-        this.nomeEvento = nomeEvento;
-        this.setorEvento = setorEvento;
-    }
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Cascade(value = org.hibernate.annotations.CascadeType.ALL)
+    private List<Tweet> tweetsEvento;
 
     //Construtor DTO
     public Evento(EventoDTO eventoDTO){
         this.nomeEvento = eventoDTO.getNomeEvento();
         this.setorEvento = eventoDTO.getSetorEvento();
+        this.tweetsEvento = eventoDTO.getTweetsEvento();
     }
 
 }
